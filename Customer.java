794faa43943041521aea7c55633f4de82343e650
Er. Amit Kumar <121847237+amit7974@users.noreply.github.com>
2023-12-20 20:50:49 +0530

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,63 @@
+package com.amit.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private int id;
+	private String fullName;
+	private String age;
+	private String email;
+	private String phone;
+	private String address;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", fullName=" + fullName + ", age=" + age + ", email=" + email + ", phone="
+				+ phone + ", address=" + address + "]";
+	}
+	
+}
